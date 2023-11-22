a8caa404b4656b955c02633eb069fbd7d71d0351
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-11-22 16:42:29 +0000

    Update customerDetails.php

--- hp website code/customerDetails.php
+++ hp website code/customerDetails.php
@@ -28,7 +28,6 @@ session_start();
 </nav>
 
     <nav class="header-nav">
-        <h1>Healthpoint Pharmacy</h1>
         <ul class="navigation-bar">
             <li><a href="homePage">Home</a></li>
             <li><a href="aboutUs.php">About Us</a></li>
