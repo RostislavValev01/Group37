ae9b34887f61bb1bc612d3353de1cc7576fb58c9
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-11-22 16:38:50 +0000

    Update productPage.php
    adding initial code

---
+++ hp website code/productPage.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Products Page</title>
+  <link rel="stylesheet" type="text/css" href="HealthPoint.css">
+  <script defer src=""></script>
+  <style></style>
+</head>
+
+<body>
+<nav class="header">
+        <a href="homePage.php"><img src="hplogo3.png" alt="Company Logo"></a>
+        <form action="/search" method="get">
+            <input type="text" name="q" placeholder="Search...">
+            <button type="submit">Go</button>
+        </form>
+        <button><a href="accountPage.php">Account</a></button>
+        <button><a href="basketPage.php">Basket</a></button>
+        <button><a href="contactUsPage.php">Contact Us</a></button>
+</nav>
+
+  <nav class="header-nav">
+    <ul class="navigation-bar">
+      <li><a href="homePage">Home</a></li>
+      <li><a href="aboutUs.php">About Us</a></li>
+      <nav class=Products>
+        <button class="dropbtn">Products</button>
+        <nav class="products-content">
+          <a href="productPage.php">Prescriptions</a>
+          <a href="productPage.php">Skin Care</a>
+          <a href="productPage.php">Hair Care</a>
+          <a href="productPage.php">Medication</a>
+        </nav>
+      </nav>
+    </ul>
+  </nav>
+
+  <h1 id="products-header">Products</h1>
+  <form class="search-bar" action="" method="get">
+    <input type="text" name="q" placeholder="Search by title...">
+    <input type="text" name="p" placeholder="Search by date...">
+    <button type="submit">Search</button>
+</form>
+
+  <table class="products-table">
+    <thead>
+      <tr>
+        <th>Product Image</th>
+        <th>Product Name</th>
+        <th>Product Price</th>
+        <th>Product Availability</th>
+        <th>Product Description</th>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+</body>
+</html>
