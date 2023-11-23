0c5e373df737e7a4e29f5e9b9205bc7a546d8a7f
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-11-23 19:44:01 +0000

    Update loginAdmin.js
    updated to include initial code - includes a function to validate a password on login, and a prompt to run when submitting form via signInAdmin. email validation to be added.

--- hp website code/loginAdmin.js
+++ hp website code/loginAdmin.js
@@ -1,1 +1,36 @@
+document.getElementById('login-form').addEventListener('submit', function (event) {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var adminID = document.getElementById('adminID').value;
 
+    checkPassword();
+
+    if (email === '' || password === '' || adminID === '') {
+        alert('All fields must be filled out');
+        event.preventDefault();
+    }
+});
+
+let form = document.getElementById("login-form");
+let first_pass = form.password;
+first_pass.onchange = checkPassword;
+
+function checkPassword() {
+    let form = document.getElementById("login-form");
+
+    let first_pass = form.password;
+    let errors = [];
+    if (first_pass.value.length < 8)
+        errors.push("Too short");
+    if (!first_pass.value.match(/^[a-zA-Z0-9]+$/))
+        errors.push("Only alphanumeric chars allowed");
+    if (!first_pass.value.match(/[a-z]/))
+        errors.push("Lower case letter required");
+    if (!first_pass.value.match(/[A-Z]/))
+        errors.push("Upper case letter required");
+    if (!first_pass.value.match(/[0-9]/)) {
+        errors.push("Number required");
+    }
+    first_pass.setCustomValidity(errors.join(', '));
+    first_pass.reportValidity();
+}
