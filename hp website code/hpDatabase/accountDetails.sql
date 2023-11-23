1c06aeb95e2163a3ce5d7503294346e3987d6f1b
mariacarla20 <148444315+mariacarla20@users.noreply.github.com>
2023-11-23 17:36:16 +0000

    Update accountDetails.sql
    Changed the data types for customer ID and admin ID

--- hp website code/hpDatabase/accountDetails.sql
+++ hp website code/hpDatabase/accountDetails.sql
@@ -1,12 +1,12 @@
 CREATE TABLE clientDetails (
-    Customer_ID  INT,
+    Customer_ID  VARCHAR(11),
     FirstName VARCHAR(40),
     Surname VARCHAR(40),
     MobileNumber INT,
     Email VARCHAR(40),
     CustomerAddress VARCHAR(40),
     DateOfBirth DATE,
-    Admin_ID INT,
+    Admin_ID VARCHAR(11),
     AdminStatus BOOLEAN,
     PRIMARY KEY (Customer_ID, Admin_ID)
     
