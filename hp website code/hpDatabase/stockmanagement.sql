1c96abd9030ae989266eb1e0d60f5b4c53ebfcd8
mariacarla20 <148444315+mariacarla20@users.noreply.github.com>
2023-11-29 15:58:06 +0000

    Update stockmanagement.sql

--- hp website code/hpDatabase/stockmanagement.sql
+++ hp website code/hpDatabase/stockmanagement.sql
@@ -1,10 +1,10 @@
-CREATE TABLE stock(
+CREATE TABLE stock2(
     SKU_number INT NOT NULL,
     Quantity INT,
     Product VARCHAR(60),
     Product_Description VARCHAR (60),
     Barcode VARCHAR(60) NOT NULL,
-    Product_Status ENUM('Pending', 'Order Confirmed', 'Shipped', 'Delivered') NOT NULL,
+    Product_Status ENUM('In stock', 'Not in Stock') NOT NULL,
     Product_Category ENUM('General', 'SkinCare', 'Vitamins_Supplements', 'DentalCare', 'Haircare'),
     PRIMARY KEY(SKU_number)
     );
