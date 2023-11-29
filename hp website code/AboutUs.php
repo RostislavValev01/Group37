e3b7aafbe48bfa0b7055c2cacd6d8afb5d072514
imebby <149668505+imebby@users.noreply.github.com>
2023-11-29 15:38:54 +0000

    Update AboutUs.php

---
+++ hp website code/AboutUs.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="HealthPoint.css">
+    <title>About Me - HealthPoint</title>
+</head>
+<body>
+    <body>
+        <nav class="banner">
+            <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+            <form action="/search" method="get">
+                <input type="text" name="q" placeholder="Search...">
+                <button type="submit">Go</button>
+            </form>
+            <div class="header">
+                <button><a href="accountPage.php">Account</a></button>
+                <button><a href="basketPage.php">Basket</a></button>
+                <button><a href="contactUsPage.php">Contact Us</a></button>
+            </div>
+        </nav>
+    <div class="header-nav">
+        <ul class="navigation-bar">
+            <li><a href="homePage">Home</a></li>
+            <li><a href="aboutUs.php">About Us</a></li>
+            <li class="Products">
+                <button class="dropbtn">Products</button>
+                <div class="products-content">
+                    <a href="productPage.php">Prescriptions</a>
+                    <a href="productPage.php">Skin Care</a>
+                    <a href="productPage.php">Hair Care</a>
+                    <a href="productPage.php">Medication</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+<div class="content">
+    <h2>Welcome to HealthPoint</h2>
+    <!-- Add more content about yourself and your organization -->
+</div>
+
+<footer>
+    <p></p>
+</footer>
+
+</body>
+</html>
