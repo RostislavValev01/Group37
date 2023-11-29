494acd6fd0e7e734cfbead7176470f10a5c72e30
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-11-29 10:47:55 +0000

    Update signInPageCustomer.php
    adjusted formatting and structure in preparation of css

--- hp website code/signInPageCustomer.php
+++ hp website code/signInPageCustomer.php
@@ -3,27 +3,29 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<meta charset="UTF-8">
 
 <head>
-    <meta charset="UTF-8">
+    <title>Customer Login</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customer Sign In Page</title>
     <link rel="stylesheet" type="text/css" href="HealthPoint.css">
-    <script defer src="loginCustomer.js"></script>
-    <style></style>
+    <script defer src="loginAdmin.js"></script>
+    <style> </style>
 </head>
 
 <body>
-    <nav class="header">
-        <a href="homePage.php"><img src="hplogo3.png" alt="Company Logo"></a>
+    <nav class="banner">
+        <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
         <form action="/search" method="get">
             <input type="text" name="q" placeholder="Search...">
             <button type="submit">Go</button>
         </form>
-        <button><a href="accountPage.php">Account</a></button>
-        <button><a href="basketPage.php">Basket</a></button>
-        <button><a href="contactUsPage.php">Contact Us</a></button>
+        <nav class="header">
+            <button><a href="accountPage.php">Account</a></button>
+            <button><a href="basketPage.php">Basket</a></button>
+            <button><a href="contactUsPage.php">Contact Us</a></button>
+        </nav>
     </nav>
 
     <nav class="header-nav">
@@ -42,21 +44,23 @@ session_start();
         </ul>
     </nav>
 
-    <div class="login">
-        <h1 id="login-header">Login</h1>
-        <?php if ($error_message): ?>
-            <p class="error">
-                <?php echo $error_message; ?>
-            </p>
-        <?php endif; ?>
-        <form id="login-form" action="" method="post"><br><br>
-            <input type="email" id="email" name="email" placeholder="Email" /><br><br>
-            <input type="password" id="password" name="password" placeholder="Password"><br><br>
-            <input type="submit" id="login" name="login" value="Login">
-            <input type="button" value="Register" id="register" class="button"
-                onclick="location.href='signupcustomer.php';">
-            <br>
-        </form>
+    <div class="content">
+        <div class="login">
+            <h1 id="login-header">Login</h1>
+            <?php if ($error_message): ?>
+                <p class="error">
+                    <?php echo $error_message; ?>
+                </p>
+            <?php endif; ?>
+            <form id="login-form" action="" method="post"><br><br>
+                <input type="email" id="email" name="email" placeholder="Email" /><br><br>
+                <input type="password" id="password" name="password" placeholder="Password"><br><br>
+                <input type="submit" id="login" name="login" value="Login">
+                <input type="button" value="Register" id="register" class="button"
+                    onclick="location.href='signupcustomer.php';">
+                <br>
+            </form>
+        </div>
     </div>
 </body>
 
