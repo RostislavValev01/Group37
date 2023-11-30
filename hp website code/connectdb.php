eacd66dda15425a83c172586bc586c1c2c0cc5b7
mariacarla20 <148444315+mariacarla20@users.noreply.github.com>
2023-11-30 10:18:33 +0000

    Update connectdb.php

--- hp website code/connectdb.php
+++ hp website code/connectdb.php
@@ -9,4 +9,27 @@
       $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch(PDOException $ex) {
       echo "Connection failed: " . $ex->getMessage();
+      exit;
   }
+
+?>
+
+
+ <?php
+$db_host = 'localhost';
+$db_name = 'hp_db'; // hp_db is the name of the database
+$username = 'root';
+$password  = '';
+
+
+try{
+    
+    $db = new PDO ("mysql:dbname=$db_name; host=$db_host", $username, $password);
+    //echo("Successfully connected to the database!");
+} catch (PDOException $ex){
+    echo("Failed to connect to the database.<br>");
+    echo($ex ->getMessage());
+    exit;
+}
+
+?> 
