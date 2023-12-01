4c8adda15d280dc492c5add2dc9485254e78eb1b
mariacarla20 <148444315+mariacarla20@users.noreply.github.com>
2023-12-01 22:51:38 +0000

    Update queries.sql

--- hp website code/hpDatabase/queries.sql
+++ hp website code/hpDatabase/queries.sql
@@ -1,20 +1,20 @@
-CREATE TABLE queries2(        --This table is in the queries database--
-    Customer_ID VARCHAR(20) NOT NULL,
-    Query_ref VARCHAR (20) NOT NULL,
-    First_Name VARCHAR(25) NOT NULL,
-    Surname VARCHAR(40) NOT NULL,
-    Email VARCHAR(100) NOT NULL,
-    Query_description VARCHAR(2000) NOT NULL
-    );
+--CREATE TABLE queries2(        --This table is in the queries database--
+   -- Customer_ID VARCHAR(20) NOT NULL,
+   --Query_ref VARCHAR (20) NOT NULL,
+   -- First_Name VARCHAR(25) NOT NULL,
+   -- Surname VARCHAR(40) NOT NULL,
+   -- Email VARCHAR(100) NOT NULL,
+   -- Query_description VARCHAR(2000) NOT NULL
+   -- );--
 
 
-ALTER TABLE queries2
-ADD PRIMARY KEY (Query_ref);
+--ALTER TABLE queries2
+--ADD PRIMARY KEY (Query_ref);
 
 --Because queries2 and clientDetails2 were two tables in two different databases I wasn't able to make Customer_ID a foreign key in the queries2 table--
 
 
-CREATE TABLE queries3(      --This table is in the account details database
+CREATE TABLE queries(      --This table is in the hp_db database
     Customer_ID VARCHAR(20) NOT NULL,
     Query_ref VARCHAR (20) NOT NULL,
     First_Name VARCHAR(25) NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE queries3(      --This table is in the account details database
     PRIMARY KEY (Query_ref)
     );
 
-ALTER TABLE queries3
+ALTER TABLE queries
 ADD FOREIGN KEY (Customer_ID) REFERENCES clientdetails2(Customer_ID);
 
 
