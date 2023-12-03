1db560d4b34e9c09beff713886f6f0fb00a3a0d6
MohammedS025 <149508116+MohammedS025@users.noreply.github.com>
2023-12-03 08:22:11 +0000

    Update OrderHistory.php

--- hp website code/OrderHistory.php
+++ hp website code/OrderHistory.php
@@ -51,6 +51,22 @@ session_start();
     </nav>
 
 
+<footer class="footer">
+        <div class="footer-section">
+            <div>
+                <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+            </div>
+            <div>
+              <p>© 2023 HealthPoint. All rights reserved.
+
+                The content, design, and images on this website are the property of HealthPoint and are protected by international copyright laws. Unauthorized use or reproduction of any materials without the express written consent of HealthPoint is strictly prohibited. HealthPoint and the HealthPoint logo are trademarks of HealthPoint.
+                
+                For inquiries regarding the use or reproduction of any content on this website, please contact us at HealthPoint@pharmacy.com</p>
+               
+            </div>
+        </div>
+    </footer>
+
    
 
 </body>
