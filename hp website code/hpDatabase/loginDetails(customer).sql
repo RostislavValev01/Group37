1974ded94dae3704e6d8561bdf45ebe4f7e65ef6
mariacarla20 <148444315+mariacarla20@users.noreply.github.com>
2023-12-05 17:47:41 +0000

    Update loginDetails(customer).sql

--- hp website code/hpDatabase/loginDetails(customer).sql
+++ hp website code/hpDatabase/loginDetails(customer).sql
@@ -1,1 +1,6 @@
-
+CREATE TABLE loginclient(
+	Customer_ID INT NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    PRIMARY KEY(Customer_ID),
+    FOREIGN KEY (Customer_ID) REFERENCES accountdetails(Customer_ID));
