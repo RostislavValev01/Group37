659099c909cf84248353dc3aad45fd452cf081dc
mariacarla20 <148444315+mariacarla20@users.noreply.github.com>
2023-12-05 17:10:17 +0000

    Update queries.sql

--- hp website code/hpDatabase/queries.sql
+++ hp website code/hpDatabase/queries.sql
@@ -32,3 +32,7 @@ INSERT INTO queries (Customer_ID, Query_ref, First_Name, Surname, Email, Query_d
 VALUES('220023', '121230', 'Emily', 'Mitchells', 'emilyM@yahoo.com', 'My order did not arrive yet');  -- at the moment only customers are allowed to make queries--
 
 
+INSERT INTO queries (First_Name, Surname, Email, Query_description)                         
+VALUES( 'Kelly', 'Parks', 'kells@yahoo.com', 'Cannot create an account'); -- now guests can make queries as well (the customer_id filed in the queries table will be NULL)--
+
+
