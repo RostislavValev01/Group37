3911f9306468b17db13d09033c5111a1236ff5c6
shifasayyed11 <149054037+shifasayyed11@users.noreply.github.com>
2023-12-06 15:20:25 +0000

    Add files via upload

---
+++ hp website code/OrderProcessing.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+
+<head>
+    <title>Order Processing</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="HealthPoint.css">
+
+</head>
+
+<body>
+    <nav class="banner">
+        <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+        <form action="/search" method="get">
+            <input type="text" name="q" placeholder="Search...">
+            <button type="submit">Go</button>
+        </form>
+        <nav class="header">
+            <button><a href="accountPage.php">Account</a></button>
+            <button><a href="basketPage.php">Basket</a></button>
+            <button><a href="Contact.php">Contact Us</a></button>
+        </nav>
+    </nav>
+
+    <nav class="header-nav">
+        <ul class="navigation-bar">
+            <li><a href="homePage">Home</a></li>
+            <li><a href="AboutUs.php">About Us</a></li>
+            <nav class=Products>
+                <button class="dropbtn">Products</button>
+                <nav class="products-content">
+                    <a href="productPage.php">Prescriptions</a>
+                    <a href="productPage.php">Skin Care</a>
+                    <a href="productPage.php">Hair Care</a>
+                    <a href="productPage.php">Medication</a>
+                </nav>
+            </nav>
+        </ul>
+    </nav>
+
+
+
+
+
+
+    <footer class="footer">
+  <div class="footer-section">
+    <div>
+      <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+    </div>
+    <div>
+      <p>© 2023 HealthPoint. All rights reserved.
+
+        The content, design, and images on this website are the property of HealthPoint and are protected by
+        international copyright laws. Unauthorized use or reproduction of any materials without the express written
+        consent of HealthPoint is strictly prohibited. HealthPoint and the HealthPoint logo are trademarks of
+        HealthPoint.
+
+        For inquiries regarding the use or reproduction of any content on this website, please contact us at
+        HealthPoint@pharmacy.com</p>
+
+    </div>
+  </div>
+</footer>
