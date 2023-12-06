a73c80aa966876b38716dbca3d70f958636038f1
imebby <149668505+imebby@users.noreply.github.com>
2023-12-06 17:24:36 +0000

    Update StockManagementPage.php

---
+++ hp website code/StockManagementPage.php
@@ -0,0 +1,139 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="HealthPoint.css">
+    <title>Inventory Management - Stock Overview</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: 'poppins', sans-serif;
+            box-sizing: border-box;
+        }
+
+        .container {
+            background-color: #b8cca3;
+            min-height: 100vh;
+            width: 100%;
+            color: rgb(0, 0, 0);
+            align-items: center;
+        }
+
+        table {
+            border: 1px solid black;
+            width: 80%;
+            margin: 20px auto;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            border: 1px solid black;
+            padding: 12px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #4caf50;
+        }
+
+        h1, h2 {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+
+    <nav class="banner">
+        <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+        <form action="/search" method="get">
+            <input type="text" name="q" placeholder="Search...">
+            <button type="submit">Go</button>
+        </form>
+        <div class="header">
+            <button><a href="accountPage.php">Account</a></button>
+            <button><a href="basketPage.php">Basket</a></button>
+            <button><a href="contactUsPage.php">Contact Us</a></button>
+        </div>
+    </nav>
+
+    <div class="header-nav">
+        <ul class="navigation-bar">
+            <li><a href="homePage.php">Home</a></li>
+            <li><a href="AboutUs.html">About Us</a></li>
+            <li class="Products">
+                <button class="dropbtn">Products</button>
+                <div class="products-content">
+                    <a href="productPage.php">Prescriptions</a>
+                    <a href="productPage.php">Skin Care</a>
+                    <a href="productPage.php">Hair Care</a>
+                    <a href="productPage.php">Medication</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <?php
+    require 'connectdb.php';
+
+    // Assuming you have a table named 'inventory'
+    $sql = "SELECT * FROM inventory";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    $inventoryDetails = $stmt->fetchAll();
+    ?>
+
+    <div class="container">
+        <h2>Inventory</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Item Number</th>
+                    <th>Item Description</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($inventoryDetails as $item): ?>
+                    <tr>
+                        <td><?= htmlspecialchars($item['part_number']) ?></td>
+                        <td><?= htmlspecialchars($item['part_description']) ?></td>
+                        <td><?= htmlspecialchars($item['quantity']) ?></td>
+                        <td><?= htmlspecialchars($item['price']) ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+<footer class="footer">
+    <div class="footer-section">
+        <div>
+            <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+        </div>
+        <div>
+            <p>© 2023 HealthPoint. All rights reserved.
+
+                The content, design, and images on this website are the property of HealthPoint and are protected by
+                international copyright laws. Unauthorized use or reproduction of any materials without the express
+                written
+                consent of HealthPoint is strictly prohibited. HealthPoint and the HealthPoint logo are trademarks of
+                HealthPoint.
+
+                For inquiries regarding the use or reproduction of any content on this website, please contact us at
+                HealthPoint@pharmacy.com</p>
+        </div>
+    </div>
+</footer>
+
+</html>
