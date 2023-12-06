e57185fb2d522010ec5bc675559184d81bc2c29b
RostislavValev01 <148444155+RostislavValev01@users.noreply.github.com>
2023-12-06 17:26:39 +0000

    Created a forgotten password page

---
+++ hp website code/forgottenpassword.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<meta charset="UTF-8">
+<head>
+<title>Forgotten Password</title>
+<script defer src="signup.js"></script>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="HealthPoint.css">
+</head>
+<style>
+</style>
+
+<body>
+
+<div class="banner">
+    <a href='#' class="logo"><img src="images/hplogo3.png" class="logo"></a>
+    <nav class="header">
+    <form action="/search" method="get">
+            <input type="text" name="q" placeholder="Search...">
+            <button type="submit">Go</button>
+        </form>
+<a href='#'>Account</a>
+<a href='#'>Basket</a>
+<a href='#'>Contact</a>
+    </nav>
+</div>
+
+<nav class="header-nav">
+    <ul class="navigation-bar">
+        <li><a href="homePage">Home</a></li>
+        <li><a href="aboutUs.php">About Us</a></li>
+        <nav class=Products>
+            <button class="dropbtn">Products</button>
+            <nav class="products-content">
+                <a href="productPage.php">Prescriptions</a>
+                <a href="productPage.php">Skin Care</a>
+                <a href="productPage.php">Hair Care</a>
+                <a href="productPage.php">Medication</a>
+            </nav>
+        </nav>
+    </ul>
+</nav> 
+<form class="register" id="registerForm"  method="post">
+<div class="content">
+
+<table>
+<tr>
+    <th>
+        <h3>Reset Your Password</h3>
+    </th>
+</tr>
+<tr><th>
+    <p>Enter the email linked to your account and we will email you the steps on how to change your password.</p>
+</th></tr>
+<tr>
+<th>
+<label for="email">Email address</label></tr>    
+</th>
+</tr>
+<tr>
+<th>
+<input type="text" id="email" name="email" placeholder="Enter your Email" required><br><br>
+</th>
+</tr>
+<tr>
+<th>
+    <input type="submit" id="submit" name="resetpass" value="Reset Password">
+
+</th>
+</tr>
+
+
+</table>
+</div>
+</form>
+
+</body>
+</html>
