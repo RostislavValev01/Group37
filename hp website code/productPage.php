0c9ab68c1e151fad3755acba47dd8d0284b312c2
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-12-07 17:13:08 +0000

    Update productPage.php - updated php code and altered css

--- hp website code/productPage.php
+++ hp website code/productPage.php
@@ -11,123 +11,26 @@ session_start();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="HealthPoint.css">
+  <link rel="stylesheet" type="text/css" href="productPage.css">
   <script defer src="loginAdmin.js"></script>
   <style>
-    form {
-      margin-bottom: 20px;
-    }
-
-    /*text within search bar settings */
-    input[type="text"],
-    select,
-    input[type="submit"] {
-      padding: 10px;
-      margin: 5px;
-    }
-
-    input[type="text"],
-    select {
-      width: 200px;
-    }
-
-    select {
-      width: 150px;
-    }
-
-    /* search bar 'search' button settings */
-    input[type="submit"] {
-      background-color: #4caf50;
-      color: white;
-      cursor: pointer;
-    }
-
-    /* headings settings */
-    h1#products-header {
-      color: #333;
-      text-align: center;
-      margin-right: 60px;
-    }
-
-    /* size of table */
-    table.products-table {
-      width: 95%;
-      border-collapse: collapse;
-      margin-top: 20px;
-      margin-left: 20px;
-    }
-
-    /* border style, size and colour */
-    table.products-table th,
-    table.products-table td {
-      border: 1px solid #ddd;
-      padding: 8px;
-      text-align: left;
-    }
-
-    /* colour of table headings */
-    table.products-table th {
-      background-color: #4caf50;
-      color: white;
-    }
-
-    /* colour of table rows */
-    table.products-table tr:hover {
-      background-color: #f5f5f5;
-    }
-
-    /* size of images */
-    table.products-table img {
-      max-width: 100px;
-      max-height: 100px;
-    }
-
-    body {
-      font-family: Arial, sans-serif;
-      background-color: #f2f2f2;
-      margin: 0;
-      padding: 0;
-    }
-
-    /* potential product-container CSS*/
-h1 {
-    text-align: center;
-}
-
-.product-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-}
-
-.product-box {
-    border: 1px solid #ddd;
-    padding: 10px;
-    margin: 10px;
-    text-align: center;
-    background-color: #fff;
-}
-
-.product-box img {
-    max-width: 100%;
-    max-height: 150px;
-}
-*/
+   
   </style>
 </head>
 
 <body>
-  <nav class="banner">
-    <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
-    <form action="/search" method="get">
-      <input type="text" name="q" placeholder="Search...">
-      <button type="submit">Go</button>
-    </form>
-    <nav class="header">
-      <button><a href="accountPage.php">Account</a></button>
-      <button><a href="basketPage.php">Basket</a></button>
-      <button><a href="contactUsPage.php">Contact Us</a></button>
+<nav class="banner">
+        <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+        <form action="/search" method="get">
+            <input type="text" name="q" placeholder="Search...">
+            <button type="submit">Go</button>
+        </form>
+        <nav class="header">
+            <button><a href="signInPageCustomer.php">Account</a></button>
+            <button><a href="Cart.php">Basket</a></button>
+            <button><a href="Contact.php">Contact Us</a></button>
+        </nav>
     </nav>
-  </nav>
 
   <nav class="header-nav">
     <ul class="navigation-bar">
@@ -136,43 +39,55 @@ h1 {
       <nav class=Products>
         <a href="productPage.php"><button class="dropbtn">Products</button></a>
         <nav class="products-content">
-          <a href="productPage.php?category=General">General Medication</a>
-          <a href="productPage.php?category=Skin Care">SkinCare</a>
-          <a href="productPage.php?category=Hair Care">HairCare</a>
-          <a href="productPage.php?category=Dental Care">DentalCare</a>
-          <a href="productPage.php?category=Vitamins_Supplements">Vitamins and Supplements</a>
+          <a href="productPage.php?Product_Category=General">General Medication</a>
+          <a href="productPage.php?Product_Category=SkinCare">SkinCare</a>
+          <a href="productPage.php?Product_Category=Haircare">HairCare</a>
+          <a href="productPage.php?Product_Category=DentalCare">DentalCare</a>
+          <a href="productPage.php?Product_Category=Vitamins_Supplements">Vitamins and Supplements</a>
         </nav>
       </nav>
     </ul>
   </nav>
 
-
   <?php
   require 'connectdb.php';
+  $sort = 'Product';
+  $order = 'asc';
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $search = $_POST['search'];
+    $searchWithWildcards = '%' . $search . '%';
     $sort = $_POST['sort'];
     $order = $_POST['order'];
 
-    $stmt = $pdo->prepare("SELECT * FROM stock3 WHERE Product LIKE :search ORDER BY $sort $order");
-    $stmt->execute(['search' => "%$search%"]);
-  } else {
-    $search = '';
-    $sort = 'Product';
-    $order = 'asc';
+    $query = "SELECT * FROM stock WHERE Product LIKE ? ORDER BY $sort $order";
+    $stmt = mysqli_prepare($con, $query);
+    mysqli_stmt_bind_param($stmt, "s", $searchWithWildcards);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
 
-    $stmt = $pdo->prepare("SELECT SKU_number, Product, Price, Product_Status, Product_Category, Product_Description FROM stock3 ORDER BY $sort $order");
-    $stmt->execute();
+  } else if (isset($_GET['Product_Category'])) {
+    $category = $_GET['Product_Category'];
+
+    $query = "SELECT SKU_number, Product, Price, Product_Status, Product_Category, Product_Description FROM stock WHERE Product_Category = ? ORDER BY $sort $order";
+    $stmt = mysqli_prepare($con, $query);
+    mysqli_stmt_bind_param($stmt, "s", $category);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+
+  } else {
+    $query = "SELECT SKU_number, Product, Price, Product_Status, Product_Category, Product_Description FROM stock ORDER BY $sort $order";
+    $result = mysqli_query($con, $query);
   }
 
-  $stock = $stmt->fetchAll();
+  $stock = mysqli_fetch_all($result, MYSQLI_ASSOC);
   ?>
 
   <div class="content">
     <form action="" method="post">
-      <input type="text" name="search"
-        value="<?= isset($search) && $search !== '' ? htmlspecialchars($search) : 'Search' ?>" placeholder="Search...">
+      <input type="text" name="search" class="search-bar"
+        value="<?= isset($search) && $search !== '' ? htmlspecialchars($search) : '' ?>" placeholder="Search...">
       <select name="sort">
         <option value="Product">Product Name</option>
         <option value="Price">Price</option>
@@ -207,20 +122,24 @@ h1 {
               <a href="indvProduct.php?id=<?php echo $product['SKU_number']; ?>">
                 <img src="productImages/<?php echo $product['SKU_number']; ?>.jpg"
                   alt="<?php echo $product['Product']; ?>" style="width:100px;height:100px;">
-                <?php echo $product['Product']; ?>
+                <p>
+                  <?php echo $product['Product']; ?>
+                </p>
               </a>
             </td>
             <td>
-              <?php echo $product['Price']; ?>
+              <?php echo '£' . $product['Price'] . '.00'; ?>
+              <form method="post" action="addtobasket.php"><br>
+                <input type="hidden" name="product_id" value="<?= $product['SKU_number']; ?>">
+                <button type="submit">Add to Basket</button>
+              </form>
             </td>
             <td>
               <?php echo $product['Product_Status']; ?>
             </td>
             <td>
-              <?php echo $product['Product_Category']; ?> form method="post" action="add_to_basket.php">
-              <input type="hidden" name="product_id" value="<?= $product['SKU_number']; ?>">
-              <button type="submit">Add to Basket</button>
-              </form>
+              <?php echo $product['Product_Description']; ?>
+
             </td>
           </tr>
         <?php endforeach; ?>
