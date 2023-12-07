b048cb4e11441713d5d842d089543aecfa9ae70f
mariacarla20 <148444315+mariacarla20@users.noreply.github.com>
2023-12-07 21:13:44 +0000

    Update orderprocessing.sql

--- hp website code/hpDatabase/orderprocessing.sql
+++ hp website code/hpDatabase/orderprocessing.sql
@@ -8,5 +8,5 @@ CREATE TABLE orderprocessing (
     FOREIGN KEY (ProductSKU) REFERENCES stock(ProductSKU)
 );
 
-INSERT INTO orderprocessing (OrderTotal, CustomerID, ProductID) VALUES ('1000.00', '220021', '1001');
-INSERT INTO orderprocessing (OrderTotal, CustomerID, ProductID) VALUES ('2000.00', '909987', '1002');
+INSERT INTO orderprocessing (OrderTotal, CustomerID, ProductSKU) VALUES ('1000.00', '220021', '1001'); 
+INSERT INTO orderprocessing (OrderTotal, CustomerID, ProductSKU) VALUES ('2000.00', '909987', '1002');
