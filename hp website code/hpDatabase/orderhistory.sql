9badb118179a7201310b39d54ca74cd5e160c263
Shivd111202 <72927508+Shivd111202@users.noreply.github.com>
2023-12-07 17:50:19 +0000

    orderhistory.sql

---
+++ hp website code/hpDatabase/orderhistory.sql
@@ -0,0 +1,24 @@
+CREATE TABLE orderhistory (
+    OrderID INT NOT NULL AUTO_INCREMENT,
+    Customer_ID INT NOT NULL,
+    ProductID INT NOT NULL,
+    ProductName VARCHAR(40) NOT NULL,
+    ProductDescription VARCHAR(255) NOT NULL,
+    Quantity INT NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    ProductSKU VARCHAR(40) NOT NULL,
+    OrderDate DATE NOT NULL,
+    OrderStatus VARCHAR(40) NOT NULL,
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (Customer_ID) REFERENCES accountDetails.sql(Customer_ID),
+    FOREIGN KEY (ProductID) REFERENCES stockmanagement.sql(ProductID)
+    );
+
+INSERT INTO orderhistory (Customer_ID, ProductID, ProductName, ProductDescription, Quantity, Price, ProductSKU, OrderDate, OrderStatus)
+VALUES ('220021','1000','Smartphone','Intel Core i5, 128GB SSD, 5.44" QHD+ Display', '1', '600.00', 'SKU-001', '2022-03-12', 'Shipped');
+
+INSERT INTO orderhistory (Customer_ID, ProductID, ProductName, ProductDescription, Quantity, Price, ProductSKU, OrderDate, OrderStatus)
+VALUES ('220021','1001','Laptop','Intel Core i7, 256GB SSD, 15.6" Full HD Display', '1', '1000.00', 'SKU-002', '2022-03-13', 'Shipped');
+
+INSERT INTO orderhistory (Customer_ID, ProductID, ProductName, ProductDescription, Quantity, Price, ProductSKU, OrderDate, OrderStatus)
+VALUES ('909987','1002','Tablet','Apple A12 Chip, 128GB SSD, 10.5" Display', '2', '500.00', 'SKU-003', '2022-03-14', 'Shipped');
