1b672a41c3436728f184f111e3482429e7df7fc7
mariacarla20 <148444315+mariacarla20@users.noreply.github.com>
2023-12-07 13:44:21 +0000

    Update stockmanagement.sql

--- hp website code/hpDatabase/stockmanagement.sql
+++ hp website code/hpDatabase/stockmanagement.sql
@@ -1,28 +1,14 @@
 CREATE TABLE stock(  --this table is in the hp_db database--
-    SKU_number INT NOT NULL,
-    Quantity INT,
-    Product VARCHAR(60),
+    SKU_number INT() NOT NULL,
+    Quantity INT(),
+    Product LONGBLOB(),
     Product_Description VARCHAR (60),
-    Barcode VARCHAR(60) NOT NULL,
+    Barcode INT NOT NULL,
     Product_Status ENUM('In stock', 'Not in Stock') NOT NULL,
     Product_Category ENUM('General', 'SkinCare', 'Vitamins_Supplements', 'DentalCare', 'Haircare'),
     PRIMARY KEY(SKU_number)
     );
 
-ALTER TABLE stock
-ALTER COLUMN SKU_number varchar();
-
-ALTER TABLE stock
-ALTER COLUMN Barcode int();
-
-INSERT INTO stock (Product)
-VALUES (LOAD_FILE("C:\Users\carla\OneDrive - Aston University\2nd Year\CS2TP\skincare product.png"));
-
-ALTER TABLE stock
-MODIFY COLUMN Product LONGBLOB();
-
-ALTER TABLE `stock` 
-MODIFY COLUMN SKU_number AUTO_INCREMENT;
 
 ALTER TABLE `stock` 
 MODIFY COLUMN SKU_number INT AUTO_INCREMENT;
