04cbc0ea6abcb499e587d8aacb71bd196681c135
mariacarla20 <148444315+mariacarla20@users.noreply.github.com>
2023-12-07 21:17:00 +0000

    Update productreviews.sql

--- hp website code/hpDatabase/productreviews.sql
+++ hp website code/hpDatabase/productreviews.sql
@@ -3,13 +3,13 @@ CREATE TABLE productreviews (
     CustomerID INT NOT NULL,
     FirstName VARCHAR(40) NOT NULL,
     LastName VARCHAR(40) NOT NULL,
-    SKUNumber INT NOT NULL,
+    ProductSKU INT NOT NULL,
     ReviewRating INT NOT NULL,
     Description TEXT NOT NULL,
     PRIMARY KEY (ReviewID),
-    FOREIGN KEY (CustomerID) REFERENCES accountDetails.sql(Customer_ID),
-    FOREIGN KEY (SKUNumber) REFERENCES stockmanagement.sql(SKU)
+    FOREIGN KEY (CustomerID) REFERENCES accountdetails(Customer_ID),
+    FOREIGN KEY (ProductSKU) REFERENCES stock(ProductSKU)
 );
 
-INSERT INTO productreviews (CustomerID, FirstName, LastName, SKUNumber, ReviewRating, Description) VALUES ('220021', 'Ellen', 'Smith', '1001', '4', 'This product is great! I highly recommend it.');
+INSERT INTO productreviews (CustomerID, FirstName, LastName, ProductSKU, ReviewRating, Description) VALUES ('220021', 'Ellen', 'Smith', '1001', '4', 'This product is great! I highly recommend it.');
 INSERT INTO productreviews (CustomerID, FirstName, LastName, SKUNumber, ReviewRating, Description) VALUES ('909987', 'Bob', 'Stone', '1002', '5', 'Awesome product! Just what I needed.');
