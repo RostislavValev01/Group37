b6b330070f6305cc8d7d507815093f3de1220f7c
imebby <149668505+imebby@users.noreply.github.com>
2023-12-08 20:25:22 +0000

    Update AboutUs.php

--- hp website code/AboutUs.php
+++ hp website code/AboutUs.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="HealthPoint.css">
-    <title>About Me - HealthPoint</title>
+    <title>About Us - HealthPoint</title>
     <style>
        
        
