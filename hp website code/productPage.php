686edb60aa2e5202016eb42c1e2a1cdadb72ea2b
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-12-08 18:18:51 +0000

    Update productPage.php - main search bar php, css, other php
    connected main banner search bar to products table and updated css. changed the main php to organise products into single boxes instead of a table.

--- hp website code/productPage.php
+++ hp website code/productPage.php
@@ -1,5 +1,41 @@
 <?php
 session_start();
+
+require 'connectdb.php';
+
+  $sort = isset($_POST['sort']) ? $_POST['sort'] : 'Product';
+  $order = isset($_POST['order']) ? $_POST['order'] : 'asc';
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $search = $_POST['search'];
+    $searchWithWildcards = '%' . $search . '%';
+
+
+    $query = "SELECT * FROM stock WHERE Product LIKE ? ORDER BY $sort $order";
+    $stmt = mysqli_prepare($con, $query);
+    mysqli_stmt_bind_param($stmt, "s", $searchWithWildcards);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+  } else if (isset($_GET['Product_Category'])) {
+    $category = $_GET['Product_Category'];
+
+    $query = "SELECT SKU_number, Product, Price, Product_Status, Product_Category, Product_Description FROM stock WHERE Product_Category = ? ORDER BY $sort $order";
+    $stmt = mysqli_prepare($con, $query);
+    mysqli_stmt_bind_param($stmt, "s", $category);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+
+  } else {
+    $sort = isset($_POST['sort']) ? $_POST['sort'] : 'Product';
+    $order = isset($_POST['order']) ? $_POST['order'] : 'asc';
+    $query = "SELECT SKU_number, Product, Price, Product_Status, Product_Category, Product_Description FROM stock ORDER BY $sort $order";
+    $result = mysqli_query($con, $query);
+    
+  }
+
+  $stock = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -11,19 +47,17 @@ session_start();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="HealthPoint.css">
-  <link rel="stylesheet" type="text/css" href="productPage.css">
-  <script defer src="loginAdmin.js"></script>
-  <style>
-
-  </style>
+  <link rel="stylesheet" type="text/css" href="productPage.css"> 
+  <script defer src="productPage\.js"></script>
 </head>
 
 <body>
   <nav class="banner">
     <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
-    <form action="/search" method="get">
-      <input type="text" name="q" placeholder="Search...">
-      <button type="submit">Go</button>
+    <form action="" method="post">
+      <input type="text" name="search" class="search-bar"
+        value="<?= isset($search) && $search !== '' ? htmlspecialchars($search) : '' ?>" placeholder="Search products...">
+        <input type="submit" value="Go">
     </form>
     <?php
     if (isset($_SESSION['loggedin'])) {
@@ -36,8 +70,8 @@ session_start();
           <button><a href="logout.php">Logout</a></button>
         </nav>
         <?php
-    } else if (isset($_SESSION['AdminStatus']) && $_SESSION['AdminStatus'] == 0) {
-      ?>
+      } else if (isset($_SESSION['AdminStatus']) && $_SESSION['AdminStatus'] == 0) {
+        ?>
           <nav class="header">
             <button><a href="CustomerAccounts.php">Account</a></button>
             <button><a href="Cart.php">Basket</a></button>
@@ -75,44 +109,6 @@ session_start();
     </ul>
   </nav>
 
-  <?php
-  require 'connectdb.php';
-  echo 'Current session ID: ' . session_id();
-  echo ' un:' . $_SESSION['Customer_ID'];
-
-  $sort = 'Product';
-  $order = 'asc';
-
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $search = $_POST['search'];
-    $searchWithWildcards = '%' . $search . '%';
-    $sort = $_POST['sort'];
-    $order = $_POST['order'];
-
-    $query = "SELECT * FROM stock WHERE Product LIKE ? ORDER BY $sort $order";
-    $stmt = mysqli_prepare($con, $query);
-    mysqli_stmt_bind_param($stmt, "s", $searchWithWildcards);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-
-  } else if (isset($_GET['Product_Category'])) {
-    $category = $_GET['Product_Category'];
-
-    $query = "SELECT SKU_number, Product, Price, Product_Status, Product_Category, Product_Description FROM stock WHERE Product_Category = ? ORDER BY $sort $order";
-    $stmt = mysqli_prepare($con, $query);
-    mysqli_stmt_bind_param($stmt, "s", $category);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-
-
-  } else {
-    $query = "SELECT SKU_number, Product, Price, Product_Status, Product_Category, Product_Description FROM stock ORDER BY $sort $order";
-    $result = mysqli_query($con, $query);
-  }
-
-  $stock = mysqli_fetch_all($result, MYSQLI_ASSOC);
-  ?>
-
   <div class="content">
     <form action="" method="post">
       <input type="text" name="search" class="search-bar"
@@ -133,64 +129,36 @@ session_start();
       <input type="submit" value="Search">
     </form>
 
-    <h1 id="products-header">Our Products</h1>
-    <table class="products-table">
-      <thead>
-        <tr>
+    <h1 id="products-header">Our Products</h1><br>
 
-          <th>Product Name</th>
-          <th>Price</th>
-          <th>Availability</th>
-          <th>Product Description</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($stock as $product): ?>
-          <tr>
-            <td>
-              <a href="indvProduct.php?id=<?php echo $product['SKU_number']; ?>">
-                <img src="productImages/<?php echo $product['SKU_number']; ?>.jpg"
-                  alt="<?php echo $product['Product']; ?>" style="width:100px;height:100px;">
-                <p>
-                  <?php echo $product['Product']; ?>
-                </p>
-              </a>
-            </td>
-            <td>
-              <?php echo '£' . $product['Price'] . '.00'; ?>
-              <form method="post" action="addtobasket.php"><br>
-                <input type="hidden" name="product_id" value="<?= $product['SKU_number']; ?>">
-                <button type="submit">Add to Basket</button>
-              </form>
-            </td>
-            <td>
-              <?php echo $product['Product_Status']; ?>
-            </td>
-            <td>
-              <?php echo $product['Product_Description']; ?>
-
-            </td>
-          </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
-
-    <!-- <div class="product-container"> 
-    <?php foreach ($stock as $product): ?>
+    <div class="products-container">
+      <?php foreach ($stock as $product): ?>
         <div class="product-box">
-            <a href="indvProduct.php?id=<?php echo $product['SKU_number']; ?>">
-                <img src="productImages/<?php echo $product['SKU_number']; ?>.jpg" alt="<?php echo $product['Product']; ?>" style="width:100px;height:100px;">
-                <h3><?php echo $product['Product']; ?></h3>
-            </a>
-            <p>Price: $<?php echo $product['Price']; ?></p>
-            <p>Availability: <?php echo ($product['Product_Status'] > 0) ? 'In Stock' : 'Out of Stock'; ?></p>
-            <button>Add to Basket</button>
+          <a href="indvProduct.php?id=<?php echo $product['SKU_number']; ?>">
+            <img src="productImages/<?php echo $product['SKU_number']; ?>.jpg" alt="<?php echo $product['Product']; ?>"
+              style="height:200px;">
+            <p class = "product-name">
+              <?php echo $product['Product']; ?>
+            </p>
+          </a>
+          <p class = "product-price">
+            <?php echo '£' . $product['Price'] . '.00'; ?>
+          </p>
+          <form method="post" action="addtobasket.php"><br>
+            <input type="hidden" name="product_id" value="<?= $product['SKU_number']; ?>">
+            <button type="submit" >Add to Basket</button>
+          </form>
+          <p>
+            <?php echo $product['Product_Status']; ?>
+          </p>
         </div>
-    <?php endforeach; ?> -->
-  </div>
+      <?php endforeach; ?>
+    </div>
 
   </div>
+
 </body>
+
 <footer class="footer">
   <div class="footer-section">
     <div>
@@ -199,9 +167,9 @@ session_start();
     <div>
 
       <p>
-        Images on this page were sourced from the following websites:
-        https://www.boots.com/
-        https://lloydspharmacy.com/
+        Images and products on this page were sourced from the following websites:<br>
+        https://www.boots.com/<br>
+        https://lloydspharmacy.com/<br><br>
         © 2023 HealthPoint. All rights reserved.
 
         The content, design, and images on this website are the property of HealthPoint and are protected by
