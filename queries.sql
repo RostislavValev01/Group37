87e02f411a9e9b27948585b7b37e228fc6ed848c
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-12-08 21:04:14 +0000

    Delete queries.sql - duplicate

--- queries.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE queries(
-    Customer_ID VARCHAR(20),
-    Query_ref VARCHAR (20),
-    First_Name VARCHAR(25),
-    Surname VARCHAR(40),
-    Email VARCHAR(100),
-    Query_description VARCHAR(2000)
-    );
