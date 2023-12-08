487758fb2c204e01d349aa6551545feb3a145f1f
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-12-08 20:22:38 +0000

    Update addtobasket.php

--- hp website code/addtobasket.php
+++ hp website code/addtobasket.php
@@ -12,7 +12,7 @@ if (isset($_POST['product_id'])) {
     $product_id = $_POST['product_id'];
     $user_id = $_SESSION['Customer_ID'];
 
-    $productQuery = "SELECT Product, Price FROM stock WHERE SKU_number = ?";
+    $productQuery = "SELECT ProductName, Price FROM stock WHERE ProductSKU = ?";
     $productStmt = mysqli_prepare($con, $productQuery);
     mysqli_stmt_bind_param($productStmt, "i", $product_id);
     mysqli_stmt_execute($productStmt);
@@ -22,7 +22,7 @@ if (isset($_POST['product_id'])) {
     if ($product) {
         $query = "INSERT INTO customerbasket (CustomerID, ProductName, ProductID, Price, Quantity) VALUES (?, ?, ?, ?, 1) ON DUPLICATE KEY UPDATE Quantity = Quantity + 1";
         $stmt = mysqli_prepare($con, $query);
-        mysqli_stmt_bind_param($stmt, "isid", $user_id, $product['Product'], $product_id, $product['Price']);
+        mysqli_stmt_bind_param($stmt, "isid", $user_id, $product['ProductName'], $product_id, $product['Price']);
         mysqli_stmt_execute($stmt);
     }
 }
