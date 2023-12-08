a3d5d20e302bcbf5d4e1814ca696f69be28f5173
Mohammedshams1 <149063928+Mohammedshams1@users.noreply.github.com>
2023-12-08 19:08:10 +0000

    Add files via upload

---
+++ basketPage.php
@@ -0,0 +1,138 @@
+
+<?php
+//include connection config
+include('connectdb.php');
+// session start
+session_start(); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<head>
+<title>Shopping Cart - Health Point</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" type="text/css" href="HealthPoint.css">
+
+</head>
+<style>
+</style>
+<body class="">
+<nav class="banner">
+    <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+    <form action="/search" method="get">
+      <input type="text" name="q" placeholder="Search...">
+      <button type="submit">Go</button>
+    </form>
+    <nav class="header">
+      <button><a href="accountPage.php">Account</a></button>
+      <button><a href="basketPage.php">Basket</a></button>
+      <button><a href="contactUsPage.php">Contact Us</a></button>
+    </nav>
+  </nav>
+
+  <nav class="header-nav">
+    <ul class="navigation-bar">
+      <li><a href="homePage.php">Home</a></li>
+      <li><a href="aboutUs.php">About Us</a></li>
+      <nav class=Products>
+        <a href="productPage.php"><button class="dropbtn">Products</button></a>
+        <nav class="products-content">
+          <a href="productPage.php?category=General">General Medication</a>
+          <a href="productPage.php?category=Skin Care">SkinCare</a>
+          <a href="productPage.php?category=Hair Care">HairCare</a>
+          <a href="productPage.php?category=Dental Care">DentalCare</a>
+          <a href="productPage.php?category=Vitamins_Supplements">Vitamins and Supplements</a>
+        </nav>
+      </nav>
+    </ul>
+  </nav>
+  <div class="content content-basket">
+
+<section class=" mt-3">
+    <h2 class="text-center">Shopping Cart</h2>
+    <div class="row justify-content-center">
+    <div style="width:60%;float:left;">
+     <div class="table-responsive">
+      <table class="table  table-bordered  table-hover" width="100%">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Action</th>
+
+    </tr>
+  </thead>
+  <tbody>
+  <?php                  
+
+  $query = 'SELECT * FROM customerbasket';
+                            $result = mysqli_query($con, $query) or die (mysqli_error($con));
+                          
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                                     
+                                    echo '<tr>';
+                                    echo '<td>'. $row['ProductID'].'</td>';
+                                    echo '<td>'. $row['ProductName'].'</td>';
+                                    echo '<td><input type="number" value="'. $row['Quantity'].'" min="0" max="10" step="1"/></td>';
+
+                                    echo '<td>'. $row['Price'].'</td>';
+                                    echo '<td>';
+
+                                    echo ' <a  type="button" class="btn btn-danger" href="crud.php?action=remove&id=' . $row['ProductID'] . '">Remove </a> </td>';
+                                    echo '</tr> ';
+                        }
+                    ?> 
+    
+  </tbody>
+</table>
+      
+     </div>
+      </div>
+      <div style="width:10%"></div>
+      <div style="width:30%;float:right;">
+<table class="table table-bordered table-hover"><tr><td colspan="2" class="text-center"><strong>Order Summary</strong></td></tr>
+
+<tbody>
+  <tr><td>Total Items</td><td>1</td></tr>
+  <tr><td>Delivery</td><td>1</td></tr>
+
+  <tr><td>Total Price</td><td>9</td></tr>
+
+</tbody></table>
+
+      </div>
+    </div>
+
+
+
+</section>
+<hr>
+<div class="mt-3 d-grid gap-2">
+
+  <button class="btn btn-success" style="float:right;"> Proceed To Checkout</button>
+</div>
+</div>
+<footer class="footer">
+  <div class="footer-section">
+    <div>
+      <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+    </div>
+    <div>
+      <p>© 2023 HealthPoint. All rights reserved.
+
+        The content, design, and images on this website are the property of HealthPoint and are protected by
+        international copyright laws. Unauthorized use or reproduction of any materials without the express written
+        consent of HealthPoint is strictly prohibited. HealthPoint and the HealthPoint logo are trademarks of
+        HealthPoint.
+
+        For inquiries regarding the use or reproduction of any content on this website, please contact us at
+        HealthPoint@pharmacy.com</p>
+
+    </div>
+  </div>
+</footer>
+</body>
+</html>
+
