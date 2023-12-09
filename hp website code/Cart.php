1b2788bae80a5f83752f30aa20324ec3ac6778ff
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-12-09 16:44:50 +0000

    Update Cart.php

--- hp website code/Cart.php
+++ hp website code/Cart.php
@@ -1,13 +1,8 @@
 <?php
 //include connection config
-       include('connection.php');
-
+include('connectdb.php');
 // session start
-session_start();
- 
-
- 
- 
+session_start(); 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,77 +10,47 @@ session_start();
 <head>
 <title>Shopping Cart - Health Point</title>
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/contact.css">
 
 </head>
 <style>
 </style>
-<body class="bg-light">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="index.php"><img src="img/hplogo3.png" height="50"></a>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link " aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " aria-current="page" href="aboutus.php">About Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " aria-current="page" href="contact.php">Contact</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Products
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-            <li><a class="dropdown-item" href="productPage.php">Prescriptions</a></li>
-            <li><a class="dropdown-item" href="productPage.php">Skin Care</a></li>
-            <li><a class="dropdown-item" href="productPage.php">Hair Care</a></li>
-            <li><a class="dropdown-item" href="productPage.php">Medication</a></li>
-          </ul>
-        </li>
-     
-      
-      </ul>
-      <form class="d-flex " action="/search" method="get">       
+<body class="">
+<nav class="banner">
+    <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+    <form action="/search" method="get">
+      <input type="text" name="q" placeholder="Search...">
+      <button type="submit">Go</button>
+    </form>
+    <nav class="header">
+      <button><a href="accountPage.php">Account</a></button>
+      <button><a href="basketPage.php">Basket</a></button>
+      <button><a href="contactUsPage.php">Contact Us</a></button>
+    </nav>
+  </nav>
 
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link " aria-current="page" href="login.php">Account/Login</a>
-        </li>
-      
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="cart.php">Basket</a>
-        </li>
-      
-        
-     
-      
-      </ul>
-    
-    </div>
-  </div>
-</nav>
-<section class="bg-dark text-light text-center banner-cart">
-<h2 class="pt-5">
-                          <span class="pb-1">
-Shopping Cart                       </span>
-                      </h2>
-</section>
-<section class="shoppingcart mt-3">
-  <div class="container-fluid ">
+  <nav class="header-nav">
+    <ul class="navigation-bar">
+      <li><a href="homePage.php">Home</a></li>
+      <li><a href="aboutUs.php">About Us</a></li>
+      <nav class=Products>
+        <a href="productPage.php"><button class="dropbtn">Products</button></a>
+        <nav class="products-content">
+          <a href="productPage.php?category=General">General Medication</a>
+          <a href="productPage.php?category=Skin Care">SkinCare</a>
+          <a href="productPage.php?category=Hair Care">HairCare</a>
+          <a href="productPage.php?category=Dental Care">DentalCare</a>
+          <a href="productPage.php?category=Vitamins_Supplements">Vitamins and Supplements</a>
+        </nav>
+      </nav>
+    </ul>
+  </nav>
+  <div class="content content-basket">
+
+<section class=" mt-3">
     <h2 class="text-center">Shopping Cart</h2>
     <div class="row justify-content-center">
-      <div class=" col-8 ">
+    <div style="width:60%;float:left;">
      <div class="table-responsive">
       <table class="table  table-bordered  table-hover" width="100%">
         <thead>
@@ -99,19 +64,33 @@ Shopping Cart                       </span>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>1</td>
-      <td>Product 1</td>
-      <td><input type="number" value="1" min="0" max="10" step="1"/></td>
-      <td>9</td>
-      <td><button class="btn btn-danger" value="">Remove</button></td>
-    </tr>
+  <?php                  
+
+  $query = 'SELECT * FROM customerbasket';
+                            $result = mysqli_query($con, $query) or die (mysqli_error($con));
+
+                                while ($row = mysqli_fetch_assoc($result)) {
+
+                                    echo '<tr>';
+                                    echo '<td>'. $row['ProductID'].'</td>';
+                                    echo '<td>'. $row['ProductName'].'</td>';
+                                    echo '<td><input type="number" value="'. $row['Quantity'].'" min="0" max="10" step="1"/></td>';
+
+                                    echo '<td>'. $row['Price'].'</td>';
+                                    echo '<td>';
+
+                                    echo ' <a  type="button" class="btn btn-danger" href="crud.php?action=remove&id=' . $row['ProductID'] . '">Remove </a> </td>';
+                                    echo '</tr> ';
+                        }
+                    ?> 
+
   </tbody>
 </table>
-      
+
      </div>
       </div>
-      <div class="col-4 float-end">
+      <div style="width:10%"></div>
+      <div style="width:30%;float:right;">
 <table class="table table-bordered table-hover"><tr><td colspan="2" class="text-center"><strong>Order Summary</strong></td></tr>
 
 <tbody>
@@ -121,33 +100,38 @@ Shopping Cart                       </span>
   <tr><td>Total Price</td><td>9</td></tr>
 
 </tbody></table>
+
+      </div>
+    </div>
+
+
+
+</section>
 <hr>
 <div class="mt-3 d-grid gap-2">
 
-  <button class="btn btn-success"> Proceed To Checkout</button>
+  <button class="btn btn-success" style="float:right;"> Proceed To Checkout</button>
 </div>
-      </div>
+</div>
+<footer class="footer">
+  <div class="footer-section">
+    <div>
+      <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
     </div>
-  </div>    
+    <div>
+      <p>© 2023 HealthPoint. All rights reserved.
 
+        The content, design, and images on this website are the property of HealthPoint and are protected by
+        international copyright laws. Unauthorized use or reproduction of any materials without the express written
+        consent of HealthPoint is strictly prohibited. HealthPoint and the HealthPoint logo are trademarks of
+        HealthPoint.
 
+        For inquiries regarding the use or reproduction of any content on this website, please contact us at
+        HealthPoint@pharmacy.com</p>
 
-</section>
-<section class="footer">
-<div class="container-fluid">
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <p class="col-md-4 mb-0 text-body-secondary">© 2023 Health Point</p>
-
- 
-  </footer>
-
-</div>
-</section>
+    </div>
+  </div>
+</footer>
 </body>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.min.js"></script>
-
-<script src="js/bootstrap-spinner.js"></script>
-<script src="js/main.js"></script>
 </html>
 
