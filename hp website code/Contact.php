8245d3e39f7a52e496baa06c14f9fb73f702a645
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-12-09 17:12:45 +0000

    Update Contact.php - css link

--- hp website code/Contact.php
+++ hp website code/Contact.php
@@ -10,7 +10,7 @@ session_start();
 <head>
 <title>Contact Us - Health Point</title>
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" type="text/css" href="HealthPoint.css">
+<link rel="stylesheet" type="text/css" href="css/contact.css">
 
 </head>
 <style>
