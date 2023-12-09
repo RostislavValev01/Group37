e05102e3a1c6f4b0ef9604d0393f4531b698224a
mariacarla20 <148444315+mariacarla20@users.noreply.github.com>
2023-12-09 21:13:39 +0000

    Update orderprocessing.sql
    updated the orderprocessing table

--- hp website code/hpDatabase/orderprocessing.sql
+++ hp website code/hpDatabase/orderprocessing.sql
@@ -11,3 +11,34 @@ CREATE TABLE orderprocessing (
 
 INSERT INTO orderprocessing (OrderTotal, CustomerID, ProductSKU, OrderStatus) VALUES ('1000.00', '220021', '1001', 'Pending'); 
 INSERT INTO orderprocessing (OrderTotal, CustomerID, ProductSKU, OrderStatus) VALUES ('2000.00', '909987', '1002'. 'Pending');
+
+
+
+
+
+
+***UPDATED ORDERPROCESSING TABLE***
+
+CREATE TABLE orderprocessing (
+    OrderNumber INT NOT NULL AUTO_INCREMENT,
+    OrderTotal DECIMAL(10, 2) NOT NULL,
+    CustomerID INT NOT NULL,
+    OrderStatus ENUM('Pending', 'Processing', 'Shipped', 'Delivered') NOT NULL,
+    Order_Description TEXT NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    Address VARCHAR(255) NOT NULL,
+    City VARCHAR(255) NOT NULL,
+    Country VARCHAR(255) NOT NULL,
+    PostCode VARCHAR(255) NOT NULL,
+    PhoneNumber VARCHAR(11) NOT NULL,
+    PRIMARY KEY (OrderNumber),
+    FOREIGN KEY (CustomerID) REFERENCES accountdetails(Customer_ID)
+
+);
+
+
+
+INSERT INTO orderprocessing(OrderTotal,CustomerID, OrderStatus, Order_Description, Email, FirstName, LastName, Address, City, Country, PostCode PhoneNumber)
+VALUES ('30','220023', 'Processing', '2x Cerave Foam Cleanser', 'emilyM@yahoo.com', 'Emily', 'Mitchells', '98 Spring Road', 'Birmingham', 'England', 'B10 0AA', '07198842650');
