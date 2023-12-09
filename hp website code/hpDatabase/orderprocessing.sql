c0bc19297969be1250dec8c659f13dbf64d19a1c
shifasayyed11 <149054037+shifasayyed11@users.noreply.github.com>
2023-12-09 15:21:22 +0000

    added 'OrderStatus' column

--- hp website code/hpDatabase/orderprocessing.sql
+++ hp website code/hpDatabase/orderprocessing.sql
@@ -3,10 +3,11 @@ CREATE TABLE orderprocessing (
     OrderTotal DECIMAL(10, 2) NOT NULL,
     CustomerID INT NOT NULL,
     ProductSKU INT NOT NULL,
+    OrderStatus STRING,
     PRIMARY KEY (OrderNumber),
     FOREIGN KEY (CustomerID) REFERENCES accountdetails(Customer_ID),
     FOREIGN KEY (ProductSKU) REFERENCES stock(ProductSKU)
 );
 
-INSERT INTO orderprocessing (OrderTotal, CustomerID, ProductSKU) VALUES ('1000.00', '220021', '1001'); 
-INSERT INTO orderprocessing (OrderTotal, CustomerID, ProductSKU) VALUES ('2000.00', '909987', '1002');
+INSERT INTO orderprocessing (OrderTotal, CustomerID, ProductSKU, OrderStatus) VALUES ('1000.00', '220021', '1001', 'Pending'); 
+INSERT INTO orderprocessing (OrderTotal, CustomerID, ProductSKU, OrderStatus) VALUES ('2000.00', '909987', '1002'. 'Pending');
