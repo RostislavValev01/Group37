c07c879fbc93dcad6c400b005a4c0c74fca973b3
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-12-09 23:00:41 +0000

    Update and rename contact.php to contactcode.php
    fixed php code to work with database and not crash

---
+++ hp website code/contactcode.php
@@ -0,0 +1,50 @@
+<?php
+//include connection config
+include('connectdb.php');
+
+// session start
+session_start();
+//$_SESSION['Customer_ID'] = 220021;
+//$_SESSION['loggedin'] = false;
+// Check if the user is already signned in then redirect him to adminpanel
+if (isset($_SESSION["Customer_ID"]) && $_SESSION["loggedin"] === true) {
+
+
+    $cID = $_SESSION["Customer_ID"];
+    $name = $_POST['fname'];
+    $lname = $_POST['lname'];
+
+    $desc = $_POST['remarks'];
+    $email = $_POST['email'];
+
+
+    $query = "INSERT INTO queries
+  (Customer_ID,First_Name,Surname,Email,Query_description)
+  VALUES ('" . $cID . "','" . $name . "','" . $lname . "','" . $email . "','" . $desc . "')";
+    mysqli_query($con, $query) or die('Error in updating Database');
+
+    $con->close();
+
+} else {
+    $cID = null;
+    $name = $_POST['fname'];
+    $lname = $_POST['lname'];
+
+    $desc = $_POST['remarks'];
+    $email = $_POST['email'];
+
+
+    $query = "INSERT INTO queries
+  (First_Name,Surname,Email,Query_description)
+  VALUES ('" . $name . "','" . $lname . "','" . $email . "','" . $desc . "')";
+    mysqli_query($con, $query) or die('Error in updating Database');
+
+    $con->close();
+
+}
+
+?>
+<script type="text/javascript">
+    alert("Query Submitted Successfull.");
+    window.location = "Contact.php";
+</script>
