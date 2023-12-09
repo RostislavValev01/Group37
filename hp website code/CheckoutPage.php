63cb7e13597759a1931596b4991dde40d6c1f442
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-12-09 21:19:16 +0000

    Update CheckoutPage.php - added php for checkout

--- hp website code/CheckoutPage.php
+++ hp website code/CheckoutPage.php
@@ -2,33 +2,40 @@
 session_start();
 
 require 'connectdb.php';
-include 'search-function.php';
+include 'checkout.php';
 
 if (isset($_GET['search'])) {
   $searchQuery = $_GET['search'];
 }
+
+
+if(!isset($_SESSION['Customer_ID'])){
+  header('Location:productPage.php');
+  } else {
+   
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <meta charset="UTF-8">
 
-<head>
+<head>  
   <title>Products</title>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="HealthPoint.css">
-  <link rel="stylesheet" type="text/css" href="Checkout.css"> 
+  <link rel="stylesheet" type="text/css" href="css/xCheckout.css">
   <script defer src="checkout.js"></script>
 </head>
 
 <body>
-  <nav class="banner">
-    <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
-    <form action="" method="get">
-      <input type="text" name="search" class="search-bar"
-        value="<?= isset($search) && $search !== '' ? htmlspecialchars($search) : '' ?>" placeholder="Search products...">
-        <input type="submit" value="Go" class="search-bar-go"></form>
+<nav class="banner">
+<a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+<form action="productPage.php" method="get">
+  <input type="text" name="search" class="search-bar"
+    value="<?= isset($search) && $search !== '' ? htmlspecialchars($search) : '' ?>" placeholder="Search products...">
+  <input type="submit" value="Go" class="search-bar-go">
+</form>
     <?php
     if (isset($_SESSION['loggedin'])) {
       if (isset($_SESSION['AdminStatus']) && $_SESSION['AdminStatus'] == 1) {
@@ -81,74 +88,80 @@ if (isset($_GET['search'])) {
 
 
   <div class="content">
-  <div class="address-container">
-    <h1>Delivery Address</h1>
-    <form action="#" method="post" id="addressForm">
+    <div class="address-container">
+      <h1>Delivery Address</h1>
+      <form action="checkout.php" method="post" id="addressForm">
         <input type="email" id="email" name="email" placeholder="Email"><br><br>
-        
+
         <div class="name-fields">
-            <input type="text" id="fname" name="fname" placeholder="First Name">
-            <input type="text" id="lname" name="lname" placeholder="Last Name"><br><br>
+          <input type="text" id="fname" name="fname" placeholder="First Name">
+          <input type="text" id="lname" name="lname" placeholder="Last Name"><br><br>
         </div>
-        
+
         <input type="text" id="address" name="address" placeholder="Address"><br><br>
-        
+
         <input type="text" id="city" name="city" placeholder="City"><br><br>
-        
+
         <div class="country-fields">
-            <input type="text" id="country" name="country" placeholder="Country">
-            <input type="text" id="postcode" name="postcode" placeholder="Postcode">
+          <input type="text" id="country" name="country" placeholder="Country">
+          <input type="text" id="postcode" name="postcode" placeholder="Postcode">
         </div><br><br>
-        
+
         <input type="tel" id="phone" name="phone" placeholder="Phone Number"><br><br>
         
-        <input type="submit" id="submit" value="Add Address">
+    <button type="submit" name="submitOrder">Submit Order</button>
     </form>
-</div>
-    <div class="payment-container">
-            <h1>Payment Method</h1>
-            <div class="payment-details">
-                <img alt="Visa/Delta/Electron" src="https://m.media-amazon.com/images/I/71DcD1e5IfL._SL40_.png" width="40px">
-                <a href="#" class="card-button" id="cardLink">Add a Credit or Debit Card</a>
-            </div>
+  </div>
+
+  <div class="payment-container">
+          <h1>Payment Method</h1>
+          <div class="payment-details">
+            <img alt="Visa/Delta/Electron" src="https://m.media-amazon.com/images/I/71DcD1e5IfL._SL40_.png"
+              width="40px">
+            <a href="#" class="card-button" id="cardLink">Add a Credit or Debit Card</a>
+          </div>
         </div>
 
         <div class="card-overlay" id="cardOverlay"></div>
         <div id="cardForm" class="card-form">
-            <form class="credit-form">
-                <h2>Card Details</h2>
-                <input type="text" id="cardholderName" name="cardholderName" placeholder="Cardholder Name"><br><br>
-                <div class="inline-fields">
-                    <input type="text" id="expiryDate" name="expiryDate" placeholder="Expiry Date (MM/YY)">
-                    <input type="text" id="cvv" name="cvv" placeholder="CVV">
-                </div><br><br>
-                <input type="text" id="cardNumber" name="cardNumber" placeholder="Card Number"><br><br>
-                <input type="submit" id="submitCard" value="Insert Payment Details">
-                <button type="button" id="closeButton" onclick="closeCardForm()">Close</button>
-            </form>
+          <form class="credit-form">
+            <h2>Card Details</h2>
+            <input type="text" id="cardholderName" name="cardholderName" placeholder="Cardholder Name"><br><br>
+            <div class="inline-fields">
+              <input type="text" id="expiryDate" name="expiryDate" placeholder="Expiry Date (MM/YY)">
+              <input type="text" id="cvv" name="cvv" placeholder="CVV">
+            </div><br><br>
+            <input type="text" id="cardNumber" name="cardNumber" placeholder="Card Number"><br><br>
+            <!-- <input type="submit" id="submitCard" value="Insert Payment Details">
+                <button type="button" id="closeButton" onclick="closeCardForm()">Close</button> -->
+          </form>
         </div>
-</div>
+    </div>
+  </div>
+  
 
-</div>
 
+  <footer class="footer">
+    <div class="footer-section">
+      <div>
+        <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+      </div>
+      <div>
+        <p>© 2023 HealthPoint. All rights reserved.
 
+          The content, design, and images on this website are the property of HealthPoint and are protected by
+          international copyright laws. Unauthorized use or reproduction of any materials without the express written
+          consent of HealthPoint is strictly prohibited. HealthPoint and the HealthPoint logo are trademarks of
+          HealthPoint.
 
-       <footer class="footer">
-        <div class="footer-section">
-            <div>
-                <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
-            </div>
-            <div>
-              <p>© 2023 HealthPoint. All rights reserved.
+          For inquiries regarding the use or reproduction of any content on this website, please contact us at
+          HealthPoint@pharmacy.com</p>
 
-                The content, design, and images on this website are the property of HealthPoint and are protected by international copyright laws. Unauthorized use or reproduction of any materials without the express written consent of HealthPoint is strictly prohibited. HealthPoint and the HealthPoint logo are trademarks of HealthPoint.
-                
-                For inquiries regarding the use or reproduction of any content on this website, please contact us at HealthPoint@pharmacy.com</p>
-               
-            </div>
-        </div>
-    </footer>
+      </div>
+    </div>
+  </footer>
 
 </body>
 
 </html>
+<?php }?>
