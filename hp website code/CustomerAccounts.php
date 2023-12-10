4e926ddc88e7fc79b4da7d537cba40dde741a68f
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-12-10 15:49:57 +0000

    Update CustomerAccounts.php - updated banner and nav bar

--- hp website code/CustomerAccounts.php
+++ hp website code/CustomerAccounts.php
@@ -58,21 +58,48 @@ if (isset($_SESSION['Customer_ID'])) {
     
 </head>
 <body>
-    <nav class="banner">
-        <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
-    <form action="" method="get">
+   <nav class="banner">
+    <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+    <form action="productPage.php" method="get">
       <input type="text" name="search" class="search-bar"
-        value="<?= isset($search) && $search !== '' ? htmlspecialchars($search) : '' ?>" placeholder="Search products...">
-        <input type="submit" value="Go" class="search-bar-go"></form>
-    
+        value="<?= isset($search) && $search !== '' ? htmlspecialchars($search) : '' ?>"
+        placeholder="Search products...">
+      <input type="submit" value="Go" class="search-bar-go">
+    </form>
+    <?php
+    if (isset($_SESSION['loggedin'])) {
+      if (isset($_SESSION['AdminStatus']) && $_SESSION['AdminStatus'] == 1) {
+        ?>
         <nav class="header">
-            <button><a href="accountPage.php">Account</a></button>
-            <button><a href="basketPage.php">Basket</a></button>
-            <button><a href="Contact.php">Contact Us</a></button>
+          <button><a href="AdminAccounts.php">Account</a></button>
+          <button><a href="Cart.php">Basket</a></button>
+          <button><a href="Contact.php">Contact Us</a></button>
+          <button><a href="logout.php">Logout</a></button>
         </nav>
-    </nav>
+        <?php
+      } else if (isset($_SESSION['AdminStatus']) && $_SESSION['AdminStatus'] == 0) {
+        ?>
+          <nav class="header">
+            <button><a href="CustomerAccounts.php">Account</a></button>
+            <button><a href="Cart.php">Basket</a></button>
+            <button><a href="Contact.php">Contact Us</a></button>
+            <button><a href="logout.php">Logout</a></button>
+          </nav>
+        <?php
+      }
+    } else {
+      ?>
+      <nav class="header">
+        <button><a href="signInPageCustomer.php">Sign In</a></button>
+        <button><a href="Cart.php">Basket</a></button>
+        <button><a href="Contact.php">Contact Us</a></button>
+      </nav>
+      <?php
+    }
+    ?>
+  </nav>
 
-    <nav class="header-nav">
+  <nav class="header-nav">
     <ul class="navigation-bar">
       <li><a href="Index.php">Home</a></li>
       <li><a href="AboutUs.php">About Us</a></li>
