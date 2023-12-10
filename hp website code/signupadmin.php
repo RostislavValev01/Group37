07011f6ace7e4cad8c6ac6d356f65f1f1dba9914
RostislavValev01 <148444155+RostislavValev01@users.noreply.github.com>
2023-12-10 15:13:14 +0000

    Both sign up forms updated
    Updated to use the same banner and header, now use connectdb and updated password hashing.

--- hp website code/signupadmin.php
+++ hp website code/signupadmin.php
@@ -15,11 +15,11 @@
 <body>
 <?php
  
- include("php/config.php");
+ require 'connectdb.php';
 if (isset($_POST["submit"])) {
 $fname = $_POST['fname'];
 $email = $_POST['email'];
-$password = $_POST['password'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 $surname = $_POST['surname'];
 $refkey = $_POST['refkey'];
 $mnumber = $_POST['mnumber'];
@@ -41,7 +41,8 @@ if (mysqli_num_rows($verify_query) !=0) {
     echo "<div class='message'>
             <p>Registration Successful!</p>
             </div> <br>";
-            "<a href='index.php'><button class='btn'>Login Now</button>";
+            header("Location: signInPageCustomer.php"); 
+            exit();
     }else{
         echo "<div class='message'>
                     <p>Invalid Admin Reference Key!</p>
@@ -50,34 +51,60 @@ if (mysqli_num_rows($verify_query) !=0) {
 }
 } else {
 ?>
-<div class="banner">
-    <a href='#' class="logo"><img src="hplogo3.png" class="logo"></a>
-    <nav class="header">
-    <form action="/search" method="get">
-            <input type="text" name="q" placeholder="Search...">
-            <button type="submit">Go</button>
-        </form>
-<a href='signInPageCustomer.php'>Account</a>
-<a href='Cart.php'>Basket</a>
-<a href='Contact.php'>Contact</a>
-    </nav>
-</div>
-
-<nav class="header-nav">
-    <ul class="navigation-bar">
-        <li><a href="homePage">Home</a></li>
-        <li><a href="aboutUs.php">About Us</a></li>
-        <nav class=Products>
-            <button class="dropbtn">Products</button>
-            <nav class="products-content">
-                <a href="productPage.php">Prescriptions</a>
-                <a href="productPage.php">Skin Care</a>
-                <a href="productPage.php">Hair Care</a>
-                <a href="productPage.php">Medication</a>
-            </nav>
+<nav class="banner">
+    <a href="Index.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+    <form action="" method="get">
+      <input type="text" name="search" class="search-bar"
+        value="<?= isset($search) && $search !== '' ? htmlspecialchars($search) : '' ?>" placeholder="Search products...">
+        <input type="submit" value="Go" class="search-bar-go"></form>
+    <?php
+    if (isset($_SESSION['loggedin'])) {
+      if (isset($_SESSION['AdminStatus']) && $_SESSION['AdminStatus'] == 1) {
+        ?>
+        <nav class="header">
+          <button><a href="AdminAccounts.php">Account</a></button>
+          <button><a href="Cart.php">Basket</a></button>
+          <button><a href="Contact.php">Contact Us</a></button>
+          <button><a href="logout.php">Logout</a></button>
         </nav>
+        <?php
+      } else if (isset($_SESSION['AdminStatus']) && $_SESSION['AdminStatus'] == 0) {
+        ?>
+          <nav class="header">
+            <button><a href="CustomerAccounts.php">Account</a></button>
+            <button><a href="Cart.php">Basket</a></button>
+            <button><a href="Contact.php">Contact Us</a></button>
+            <button><a href="logout.php">Logout</a></button>
+          </nav>
+        <?php
+      }
+    } else {
+      ?>
+      <nav class="header">
+        <button><a href="signInPageCustomer.php">Sign In</a></button>
+        <button><a href="Cart.php">Basket</a></button>
+        <button><a href="Contact.php">Contact Us</a></button>
+      </nav>
+      <?php
+    }
+    ?>
+  </nav>
+  <nav class="header-nav">
+    <ul class="navigation-bar">
+      <li><a href="Index.php">Home</a></li>
+      <li><a href="AboutUs.php">About Us</a></li>
+      <nav class=Products>
+        <a href="productPage.php"><button class="dropbtn">Products</button></a>
+        <nav class="products-content">
+          <a href="productPage.php?Product_Category=General">General Medication</a>
+          <a href="productPage.php?Product_Category=SkinCare">SkinCare</a>
+          <a href="productPage.php?Product_Category=Haircare">HairCare</a>
+          <a href="productPage.php?Product_Category=DentalCare">DentalCare</a>
+          <a href="productPage.php?Product_Category=Vitamins_Supplements">Vitamins and Supplements</a>
+        </nav>
+      </nav>
     </ul>
-</nav> 
+  </nav>
 <form class="register" id="registerForm"  method="post">
 <div class="content">
 
