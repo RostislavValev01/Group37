42e9f7b62cceb081bd69687f724ad8fb328785e8
MohammedS025 <149508116+MohammedS025@users.noreply.github.com>
2023-12-10 17:30:04 +0000

    Update CheckoutPage.php

--- hp website code/CheckoutPage.php
+++ hp website code/CheckoutPage.php
@@ -14,6 +14,19 @@ if (!isset($_SESSION['Customer_ID'])) {
 } else {
 
   ?>
+  <!DOCTYPE html>
+  <html lang="en">
+  <meta charset="UTF-8">
+
+  <head>
+    <title>Checkout</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="HealthPoint.css">
+    <link rel="stylesheet" type="text/css" href="css/Checkout.css">
+    <script defer src="Checkout.js"></script>
+  </head>
+
 
   <nav class="banner">
     <a href="Index.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
