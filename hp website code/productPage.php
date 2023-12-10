c17360ba662325ef5a8f4df593c6767116632d00
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2023-12-10 20:22:43 +0000

    Update productPage.php

--- hp website code/productPage.php
+++ hp website code/productPage.php
@@ -18,7 +18,7 @@ if (isset($_GET['search'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="HealthPoint.css">
-  <link rel="stylesheet" type="text/css" href="productPage.css"> 
+  <link rel="stylesheet" type="text/css" href="css/productPage.css"> 
   <script defer src="productPage\.js"></script>
 </head>
 
