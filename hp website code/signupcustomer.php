f922efa0929f33525047d767b5ae0095e520d801
imebby <149668505+imebby@users.noreply.github.com>
2024-03-02 10:32:15 +0000

    Update signupcustomer.php

--- hp website code/signupcustomer.php
+++ hp website code/signupcustomer.php
@@ -180,7 +180,7 @@ else{
 <footer class="footer">
     <div class="footer-section">
         <div>
-            <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+           <img src="hplogo3.png" class="logo" alt="Company Logo"></a>
         </div>
         <div>
             <p>© 2023 HealthPoint. All rights reserved.
