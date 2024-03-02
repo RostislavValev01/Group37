2497f57ee611a8c4d637ee8d71ef50aa35aeeda6
imebby <149668505+imebby@users.noreply.github.com>
2024-03-02 10:29:50 +0000

    Update OrderProcessing.php

--- hp website code/OrderProcessing.php
+++ hp website code/OrderProcessing.php
@@ -91,7 +91,7 @@ $result = $con->query($sql);
     <footer class="footer">
   <div class="footer-section">
     <div>
-      <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+     <img src="hplogo3.png" class="logo" alt="Company Logo"></a>
     </div>
     <div>
       <p>© 2023 HealthPoint. All rights reserved.
