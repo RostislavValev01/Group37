c5c03774d7237bf0a9667740c06d54012ac1bb5d
imebby <149668505+imebby@users.noreply.github.com>
2024-03-02 10:29:22 +0000

    Update CustomerAccounts.php

--- hp website code/CustomerAccounts.php
+++ hp website code/CustomerAccounts.php
@@ -168,7 +168,7 @@ if (isset($_SESSION['Customer_ID'])) {
     <footer class="footer">
   <div class="footer-section">
     <div>
-      <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+     <img src="hplogo3.png" class="logo" alt="Company Logo"></a>
     </div>
     <div>
       <p>© 2023 HealthPoint. All rights reserved.
