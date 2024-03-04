e076935011a107b439af0908bf766e20b860fbbe
mariacarla20 <148444315+mariacarla20@users.noreply.github.com>
2024-03-04 13:38:29 +0000

    Update orderhistory.sql
    Deleted some of the Insert into commands as they weren't relevant to our website

--- hp website code/hpDatabase/orderhistory.sql
+++ hp website code/hpDatabase/orderhistory.sql
@@ -31,14 +31,6 @@ VALUES ('220022','Difflam Mouth Spray','Suitable for adults, the elderly and chi
 
 
 
-INSERT INTO orderhistory (Customer_ID, ProductID, ProductName, ProductDescription, Quantity, Price, ProductSKU, OrderDate, OrderStatus)
-VALUES ('220021','1000','Smartphone','Intel Core i5, 128GB SSD, 5.44" QHD+ Display', '1', '600.00', 'SKU-001', '2022-03-12', 'Shipped');
-
-INSERT INTO orderhistory (Customer_ID, ProductID, ProductName, ProductDescription, Quantity, Price, ProductSKU, OrderDate, OrderStatus)
-VALUES ('220021','1001','Laptop','Intel Core i7, 256GB SSD, 15.6" Full HD Display', '1', '1000.00', 'SKU-002', '2022-03-13', 'Shipped');
-
-INSERT INTO orderhistory (Customer_ID, ProductID, ProductName, ProductDescription, Quantity, Price, ProductSKU, OrderDate, OrderStatus)
-VALUES ('909987','1002','Tablet','Apple A12 Chip, 128GB SSD, 10.5" Display', '2', '500.00', 'SKU-003', '2022-03-14', 'Shipped');
 
 
 
