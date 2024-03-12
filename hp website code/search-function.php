52c7025af1a9e01b26bb1427995064b0cfb643f6
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2024-03-12 16:43:11 +0000

    Update search-function.php
    updated for 'StockManagementPage' search and filter function (quantity and barcode)

--- hp website code/search-function.php
+++ hp website code/search-function.php
@@ -17,15 +17,16 @@ if (isset($_GET['search'])) {
 } else if (isset($_GET['Product_Category'])) {
     $category = $_GET['Product_Category'];
 
-    $query = "SELECT ProductSKU, ProductName, Price, Product_Status, Product_Category, Product_Description FROM stock WHERE Product_Category = ? ORDER BY $sort $order";
+    $query = "SELECT ProductSKU, ProductName, Quantity, Price, Barcode, Product_Status, Product_Category, Product_Description FROM stock WHERE Product_Category = ? ORDER BY $sort $order";
     $stmt = mysqli_prepare($con, $query);
     mysqli_stmt_bind_param($stmt, "s", $category);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
 
 } else {
-    $query = "SELECT ProductSKU, ProductName, Price, Product_Status, Product_Category, Product_Description FROM stock ORDER BY $sort $order";
+    $query = "SELECT ProductSKU, ProductName, Quantity, Price, Barcode, Product_Status, Product_Category, Product_Description FROM stock ORDER BY $sort $order";
     $result = mysqli_query($con, $query);
 }
 
 $stock = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
