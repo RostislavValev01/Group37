f37cdc7169ed81649ac126ad89518d347b444679
RostislavValev01 <148444155+RostislavValev01@users.noreply.github.com>
2024-03-13 13:21:56 +0000

    Added a logic that automatically increments the Admin_ID field on the database.

--- hp website code/signupadmin.php
+++ hp website code/signupadmin.php
@@ -4,10 +4,10 @@
 <meta charset="UTF-8">
 <head>
 <title>Sign Up Admin</title>
-<script defer src="signup.js"></script>
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <link rel="stylesheet" href="HealthPoint.css">
 <link rel="stylesheet" href ="signup.css">
+<script defer src="signup.js"></script>
 
 </head>
 
@@ -15,42 +15,54 @@
 <body>
 <?php
  
- require 'connectdb.php';
+require 'connectdb.php';
 if (isset($_POST["submit"])) {
-$fname = $_POST['fname'];
-$email = $_POST['email'];
-$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$surname = $_POST['surname'];
-$refkey = $_POST['refkey'];
-$mnumber = $_POST['mnumber'];
-$address = $_POST['address'];
-$dob = $_POST['dob'];
+    $fname = $_POST['fname'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $surname = $_POST['surname'];
+    $refkey = $_POST['refkey'];
+    $mnumber = $_POST['mnumber'];
+    $address = $_POST['address'];
+    $dob = $_POST['dob'];
+    $adminid = $_POST['Admin_ID'];
 
+    // Get the maximum AdminID currently in the table
+    $result = mysqli_query($con, "SELECT MAX(Admin_ID) AS max_id FROM accountdetails");
+    $row = mysqli_fetch_assoc($result);
+    $max_id = $row['max_id'];
 
-//verify the email
-$verify_query = mysqli_query($con, "SELECT Email FROM accountdetails WHERE Email='$email'");
-if (mysqli_num_rows($verify_query) !=0) {
-    echo "<div class='message'>
-            <p>This email is used, Try another One Please!</p>
-            </div> <br>";
-            "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
-}else{
-    $admin_reference_key = 'admin123';
-    if ($refkey === $admin_reference_key) {
-    mysqli_query($con, "INSERT INTO accountdetails(FirstName, Surname, MobileNumber, Email, Password, CustomerAddress, DateOfBirth, AdminStatus) VALUES('$fname', '$surname', '$mnumber', '$email', '$password', '$address', '$dob', 1)" ) or die("Error Occured");
-    echo "<div class='message'>
-            <p>Registration Successful!</p>
-            </div> <br>";
+    // Calculate the next AdminID in the database
+    $next_id = $max_id + 1;
+
+    //verify the email
+    $verify_query = mysqli_query($con, "SELECT Email FROM accountdetails WHERE Email='$email'");
+    if (mysqli_num_rows($verify_query) !=0) {
+        echo "<div class='message'>
+                <p>This email is used, Try another One Please!</p>
+              </div> <br>";
+        echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
+    } else {
+        $admin_reference_key = 'admin123';
+        if ($refkey === $admin_reference_key) {
+            
+            mysqli_query($con, "INSERT INTO accountdetails(FirstName, Surname, MobileNumber, Email, Password, CustomerAddress, DateOfBirth, Admin_ID, AdminStatus) VALUES('$fname', '$surname', '$mnumber', '$email', '$password', '$address', '$dob', '$next_id', 1)" ) or die("Error Occured");
+            echo "<div class='message'>
+                    <p>Registration Successful!</p>
+                  </div> <br>";
             header("Location: signInPageAdmin.php"); 
             exit();
-    }else{
-        echo "<div class='message'>
-                    <p>Invalid Admin Reference Key!</p>
-                  </div> <br>";
+        } else {
+            echo "<div class='message'>
+                        <p>Invalid Admin Reference Key!</p>
+                      </div> <br>";
+        }
     }
 }
-} else {
 ?>
+
+
+
 <nav class="banner">
     <a href="Index.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
     <form action="" method="get">
@@ -108,95 +120,93 @@ if (mysqli_num_rows($verify_query) !=0) {
 <form class="register" id="registerForm"  method="post">
 <div class="content">
 
-<table class="registration-table">
-                <tr>
-                    <th colspan="2">
-                        <h3>Create an Admin account</h3>
-                    </th>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="fname">First name:</label>
-                    </td>
-                    <td>
-                        <input type="text" id="fname" name="fname" placeholder="First Name" required><br><br>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="surname">Surname:</label>
-                    </td>
-                    <td>
-                        <input type="text" id="surname" name="surname" placeholder="Surname" required><br><br>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="email">Email:</label>
-                    </td>
-                    <td>
-                        <input type="text" id="email" name="email" placeholder="Email" required><br><br>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="password">Password:</label>
-                    </td>
-                    <td>
-                        <input type="password" id="password" name="password" placeholder="Password" required><br><br>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="address">Address:</label>
-                    </td>
-                    <td>
-                        <input type="text" id="address" name="address" placeholder="Address" required><br><br>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="mnumber">Mobile Number:</label>
-                    </td>
-                    <td>
-                        <input type="number" id="mnumber" name="mnumber" placeholder="Mobile Number" required><br><br>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="dob">Date of Birth:</label>
-                    </td>
-                    <td>
-                        <input type="date" id="dob" name="dob" placeholder="Date of Birth" required><br><br>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="refkey">Admin Reference Key:</label>
-                    </td>
-                    <td>
-                        <input type="password" id="refkey" name="refkey" placeholder="Reference Key" required><br><br>
-                    </td>
-                </tr>
-                <tr>
-    <td colspan="2">
-        <a href="signupcustomer.php" class="customer-link">Back to customer registration</a>
-    </td>
-</tr>
-                <tr>
-                    <td colspan="2">
-                        <input type="submit" id="submit" name="submit" value="Create new Account">
-                    </td>
-                </tr>
-            </table>
-</div>
+<form class="register" id="registerForm" action="" method="post">
+    <div class="content">
+        <table class="registration-table">
+            <tr>
+                <th colspan="2">
+                    <h3>Create an Admin account</h3>
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <label for="fname">First name:</label>
+                </td>
+                <td>
+                    <input type="text" id="fname" name="fname" placeholder="First Name" required><br><br>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="surname">Surname:</label>
+                </td>
+                <td>
+                    <input type="text" id="surname" name="surname" placeholder="Surname" required><br><br>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="email">Email:</label>
+                </td>
+                <td>
+                    <input type="email" id="email" name="email" placeholder="Email" required><br><br>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="password">Password:</label>
+                </td>
+                <td>
+                    <input type="password" id="password" name="password" placeholder="Password" required><br><br>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="address">Address:</label>
+                </td>
+                <td>
+                    <input type="text" id="address" name="address" placeholder="Address" required><br><br>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="mnumber">Mobile Number:</label>
+                </td>
+                <td>
+                    <input type="text" id="mnumber" name="mnumber" placeholder="Mobile Number" required><br><br>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="dob">Date of Birth:</label>
+                </td>
+                <td>
+                    <input type="date" id="dob" name="dob" placeholder="Date of Birth" required><br><br>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="refkey">Admin Reference Key:</label>
+                </td>
+                <td>
+                    <input type="password" id="refkey" name="refkey" placeholder="Reference Key" required><br><br>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" id="submit" name="submit" value="Create new Account">
+                </td>
+            </tr>
+        </table>
+    </div>
 </form>
+
 
 </body>
 <footer class="footer">
     <div class="footer-section">
         <div>
-           <img src="hplogo3.png" class="logo" alt="Company Logo"></a>
+            <a href="homePage.php"><img src="hplogo3.png" class="logo" alt="Company Logo"></a>
         </div>
         <div>
             <p>© 2023 HealthPoint. All rights reserved.
@@ -211,7 +221,7 @@ if (mysqli_num_rows($verify_query) !=0) {
                 HealthPoint@pharmacy.com</p>
 
         </div>
-        <?php } ?>
+        <?php  ?>
     </div>
 </footer>
 </html>
