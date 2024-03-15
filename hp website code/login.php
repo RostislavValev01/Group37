5f320762c3a2c1d1ceca7d1629fba476dd30ae35
dylan3489 <149049411+dylan3489@users.noreply.github.com>
2024-03-15 15:14:02 +0000

    Update login.php

--- hp website code/login.php
+++ hp website code/login.php
@@ -13,24 +13,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->bind_param("s", $email);
         $stmt->execute();
         $result = $stmt->get_result();
-        $user = $result->fetch_assoc();
 
-        $verifyResult = password_verify($password, $user['Password']);
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            $verifyResult = password_verify($password, $user['Password']);
 
+            if ($user !== null && $verifyResult) { 
+                $_SESSION['Customer_ID'] = $user['Customer_ID'];
+                $_SESSION['AdminStatus'] = 0;
+                $_SESSION['loggedin'] = true;
+            
+                echo '<script>alert("Hi ' . $user['Email'] . ', you are now logged in!");</script>';
+                echo '<script>window.location.href = "Index.php";</script>';
+                exit();
+            } else {
+                echo '<script>alert("Invalid email or password.");</script>';
+                echo '<script>window.location.href = "signInPageCustomer.php";</script>';
 
-        if ($user && $password == $verifyResult) { 
-            $_SESSION['Customer_ID'] = $user['Customer_ID'];
-            $_SESSION['AdminStatus'] = 0;
-            $_SESSION['loggedin'] = true;
-
-            echo '<script>alert("Hi ' . $user['Email'] . ', you are now logged in!");</script>';
-            echo '<script>window.location.href = "Index.php";</script>';
-
-            exit();
+                exit();
+            }
         } else {
-            $_SESSION['error'] = "Invalid email or password.";
-            echo $_SESSION['error'];
-            header('Location: signInPageCustomer.php');
+            echo '<script>alert("Invalid email or password.");</script>';
+            echo '<script>window.location.href = "signInPageCustomer.php";</script>';
             exit();
         }
     }
@@ -40,3 +44,4 @@ if ($stmt instanceof mysqli_stmt) {
     $stmt->close();
 }
 $con->close();
+
